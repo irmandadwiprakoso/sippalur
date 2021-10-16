2af509903e6f129f0f0336d5cd4366523eb01171
irmandadwiprakoso <irmanda7@gmail.com>
2021-10-16 11:20:07 +0700

    update

--- config/app.php
+++ config/app.php
@@ -230,7 +230,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        // 'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Datatables' => Yajra\Datatables\DatatablesServiceProvider::class
     ],
 
 ];
