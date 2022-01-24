cb051d8d971d71f27408c1e723e063ef78471c8d
irmandadwiprakoso <irmanda7@gmail.com>
2022-01-25 06:50:17 +0700

    update kependudukan

--- app/Http/Controllers/KependudukanController.php
+++ app/Http/Controllers/KependudukanController.php
@@ -21,117 +21,12 @@ class KependudukanController extends Controller
     public function index()
     {
 
-        if(auth()->user()->role == 'superadmin')
+        if(auth()->user()->role != 'user')
         {
-            $kependudukan = Kependudukan::orderbyRaw('rw_id', 'DESC')->get();
+            $kependudukan = Kependudukan::orderby('rw_id', 'asc')->get();
+        }else{
+            $kependudukan = Kependudukan::where('rw_id', '=', auth()->user()->rw_id)->orderby('rt_id', 'asc')->get();
         }
-        if(auth()->user()->role == 'admin_pemtibum')
-        {
-            $kependudukan = Kependudukan::orderbyRaw('rw_id', 'DESC')->get();
-        }
-        if(auth()->user()->role == 'lurah')
-        {
-            $kependudukan = Kependudukan::orderbyRaw('rw_id', 'DESC')->get();
-        }
-        if(auth()->user()->role == 'admin')
-        {
-            $kependudukan = Kependudukan::orderbyRaw('rw_id', 'DESC')->get();
-        }
-
-        if (auth()->user()->rw_id == '1')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '1')->get();
-        }
-        if (auth()->user()->rw_id == '2')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '2')->get();
-        }
-        if (auth()->user()->rw_id == '3')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '3')->get();
-        }
-        if (auth()->user()->rw_id == '4')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '4')->get();
-        }
-        if (auth()->user()->rw_id == '5')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '5')->get();
-        }
-        if (auth()->user()->rw_id == '6')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '6')->get();
-        }
-        if (auth()->user()->rw_id == '7')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '7')->get();
-        }
-        if (auth()->user()->rw_id == '8')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '8')->get();
-        }
-        if (auth()->user()->rw_id == '9')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '9')->get();
-        }
-        if (auth()->user()->rw_id == '10')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '10')->get();
-        }
-        if (auth()->user()->rw_id == '11')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '11')->get();
-        }
-        if (auth()->user()->rw_id == '12')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '12')->get();
-        }
-        if (auth()->user()->rw_id == '13')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '13')->get();
-        }
-        if (auth()->user()->rw_id == '14')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '14')->get();
-        }
-        if (auth()->user()->rw_id == '15')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '15')->get();
-        }
-        if (auth()->user()->rw_id == '16')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '16')->get();
-        }
-        if (auth()->user()->rw_id == '17')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '17')->get();
-        }
-        if (auth()->user()->rw_id == '18')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '18')->get();
-        }
-        if (auth()->user()->rw_id == '19')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '19')->get();
-        }
-        if (auth()->user()->rw_id == '20')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '20')->get();
-        }
-        if (auth()->user()->rw_id == '21')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '21')->get();
-        }
-        if (auth()->user()->rw_id == '22')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '22')->get();
-        }
-        if (auth()->user()->rw_id == '23')
-        {
-            $kependudukan = Kependudukan::where('rw_id', '=', '23')->get();
-        }
-
-
         return view('kependudukan.index', ['kependudukan' => $kependudukan]);
     }
     public function exportkependudukan() 
@@ -254,93 +149,10 @@ class KependudukanController extends Controller
     public function getdatakependudukan()
     {
         ////////////////////////// AKUN ADMIN /////////////////////////////
-        if(auth()->user()->role == 'superadmin'){
+        if(auth()->user()->role != 'user'){
             $kependudukan = Kependudukan::select('kependudukan.*')->orderBy('rw_id', 'asc')->orderBy('rt_id', 'asc');
-        }
-        if(auth()->user()->role == 'admin'){
-            $kependudukan = Kependudukan::select('kependudukan.*')->orderBy('rw_id', 'asc')->orderBy('rt_id', 'asc');
-        }
-        if(auth()->user()->role == 'sekret'){
-            $kependudukan = Kependudukan::select('kependudukan.*')->orderBy('rw_id', 'asc')->orderBy('rt_id', 'asc');
-        }
-        if(auth()->user()->role == 'kessos'){
-            $kependudukan = Kependudukan::select('kependudukan.*')->orderBy('rw_id', 'asc')->orderBy('rt_id', 'asc');
-        }
-        if(auth()->user()->role == 'pemtibum'){
-            $kependudukan = Kependudukan::select('kependudukan.*')->orderBy('rw_id', 'asc')->orderBy('rt_id', 'asc');
-        }
-        if(auth()->user()->role == 'permasbang'){
-            $kependudukan = Kependudukan::select('kependudukan.*')->orderBy('rw_id', 'asc')->orderBy('rt_id', 'asc');
-        }
-        ///////////// AKUN PAMOR //////////////////////////////////
-        if (auth()->user()->rw_id == '1'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '1')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '2'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '2')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '3'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '3')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '4'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '4')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '5'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '5')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '6'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '6')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '7'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '7')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '8'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '8')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '9'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '9')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '10'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '10')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '11'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '11')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '12'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '12')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '13'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '13')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '14'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '14')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '15'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '15')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '16'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '16')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '17'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '17')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '18'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '18')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '19'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '19')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '20'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '20')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '21'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '21')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '22'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '22')->orderBy('rt_id', 'asc');
-        }
-        if (auth()->user()->rw_id == '23'){
-        $kependudukan = Kependudukan::where('rw_id', '=', '23')->orderBy('rt_id', 'asc');
+        }else {
+            $kependudukan = Kependudukan::where('rw_id', '=', auth()->user()->rw_id )->orderBy('rt_id', 'asc');
         }
 
         // $kependudukan = Kependudukan::select('kependudukan.*');
